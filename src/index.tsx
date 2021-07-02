3ea723655cb7b63ac32f9db9a4e11ba88b4ce45b
takeru.hashimoto <hstake25@gmail.com>
2021-07-03 08:41:06 +0900

    functional component 入門

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+type Foo = JSX.IntrinsicAttributes;
+
+// Appコンポーネントにmessageを受け入れをしなくても、keyはエラーにならない。
+// <App message="hello react!" key="1" />
+// JSX.IntrinsicAttributes で keyオプショナルだから
+ReactDOM.render(<App message="hello react!" />, document.getElementById('root'));
